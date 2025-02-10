3759a24bf5102ce08f943553f0d4e2c9fbf8ec5c
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-10 18:07:20 -0500

    feat: add global settings in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(app()->isProduction());
+        Model::shouldBeStrict(! app()->isProduction());
+        URL::forceHttps(app()->isProduction());
     }
 }
