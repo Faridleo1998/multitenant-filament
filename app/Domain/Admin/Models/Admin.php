829198121559e0912da0243abfa16c1d90a3f486
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-11 21:53:20 -0500

    feat: implements FilamentUser and add method in admin model

--- app/Domain/Admin/Models/Admin.php
+++ app/Domain/Admin/Models/Admin.php
@@ -3,12 +3,14 @@
 namespace App\Domain\Admin\Models;
 
 use App\Domain\Admin\Models\Attributes\AdminAttributes;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use AdminAttributes;
     use HasRoles, Notifiable, SoftDeletes;
@@ -23,6 +25,15 @@ class Admin extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return str_ends_with($this->email, config('system.domain'));
+        }
+
+        return true;
+    }
 
     protected function casts(): array
     {
