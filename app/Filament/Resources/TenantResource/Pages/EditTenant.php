f60551728766a3d8f665a2c2cc7c82b2b1f2d7d1
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 18:23:03 -0500

    feat: add combined relation manager in edit and view pages in tenant resource

--- app/Filament/Resources/TenantResource/Pages/EditTenant.php
+++ app/Filament/Resources/TenantResource/Pages/EditTenant.php
@@ -8,4 +8,19 @@ use Filament\Resources\Pages\EditRecord;
 class EditTenant extends EditRecord
 {
     protected static string $resource = TenantResource::class;
+
+    public function hasCombinedRelationManagerTabsWithContent(): bool
+    {
+        return true;
+    }
+
+    public function getContentTabIcon(): ?string
+    {
+        return 'heroicon-o-home-modern';
+    }
+
+    public function getContentTabLabel(): ?string
+    {
+        return ucfirst(self::$resource::getModelLabel());
+    }
 }
--- app/Filament/Resources/TenantResource/Pages/ViewTenant.php
+++ app/Filament/Resources/TenantResource/Pages/ViewTenant.php
@@ -8,4 +8,19 @@ use Filament\Resources\Pages\ViewRecord;
 class ViewTenant extends ViewRecord
 {
     protected static string $resource = TenantResource::class;
+
+    public function hasCombinedRelationManagerTabsWithContent(): bool
+    {
+        return true;
+    }
+
+    public function getContentTabIcon(): ?string
+    {
+        return 'heroicon-o-home-modern';
+    }
+
+    public function getContentTabLabel(): ?string
+    {
+        return ucfirst(self::$resource::getModelLabel());
+    }
 }
