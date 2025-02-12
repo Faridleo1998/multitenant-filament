6a1adf5c29668a7cce96f25310cccaba5e08a627
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 16:19:05 -0500

    feat: add attributes, relationships and fields in tenant model

--- app/Domain/Tenant/Models/Tenant.php
+++ app/Domain/Tenant/Models/Tenant.php
@@ -2,6 +2,8 @@
 
 namespace App\Domain\Tenant\Models;
 
+use App\Domain\Tenant\Models\Attributes\TenantAttributes;
+use App\Domain\Tenant\Models\Relationships\TenantRelationships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
@@ -10,12 +12,19 @@ use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase, HasFactory;
+    use TenantAttributes, TenantRelationships;
 
     public $incrementing = false;
 
     protected $keyType = 'string';
 
     protected $primaryKey = 'id';
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 
     public static function getCustomColumns(): array
     {
@@ -26,6 +35,9 @@ class Tenant extends BaseTenant implements TenantWithDatabase
             'phone',
             'email_contact',
             'address',
+            'country_id',
+            'state_id',
+            'city_id',
         ];
     }
 }
