3396d24fefdee34312c82e29e63c5f10e5aa3552
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 09:38:56 -0500

    feat: modify should be strict in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         DB::prohibitDestructiveCommands(app()->isProduction());
-        Model::shouldBeStrict(app()->isLocal());
         URL::forceHttps(app()->isProduction());
+        $this->shouldBeStrict(app()->isLocal());
+    }
+
+    public function shouldBeStrict($appIsLocal)
+    {
+        Model::preventLazyLoading($appIsLocal);
+        Model::preventAccessingMissingAttributes($appIsLocal);
     }
 }
