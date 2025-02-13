2a61c85a5694625d7da6c2d6e8dc9304b578e207
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 20:34:52 -0500

    feat: add database and shield tenant seeders

---
+++ database/seeders/Tenant/DatabaseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders\Tenant;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call([
+            ShieldSeeder::class,
+        ]);
+    }
+}
