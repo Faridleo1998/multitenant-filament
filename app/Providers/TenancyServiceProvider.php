31a5c0ecee11b06428881d6d54f5f4683b909580
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 20:35:16 -0500

    feat: add database seed job in tenancy service provider

--- app/Providers/TenancyServiceProvider.php
+++ app/Providers/TenancyServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Domain\Tenant\Jobs\SeedDatabase;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
@@ -27,7 +28,7 @@ class TenancyServiceProvider extends ServiceProvider
                 JobPipeline::make([
                     Jobs\CreateDatabase::class,
                     Jobs\MigrateDatabase::class,
-                    // Jobs\SeedDatabase::class,
+                    SeedDatabase::class,
 
                     // Your own jobs to prepare the tenant.
                     // Provision API keys, create S3 buckets, anything you want!
