3030f9b89563d0793761ce1aa42b31d42fea85b1
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-13 11:43:37 -0500

    chore(i18n): add es labels

---
+++ lang/es/labels.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'city' => 'Ciudad',
+    'country' => 'País',
+    'created_at' => 'Fecha de creación',
+    'email' => 'Correo electrónico',
+    'identification' => 'Identificación',
+    'name' => 'Nombre',
+    'phone' => 'Teléfono',
+    'full_name' => 'Nombre completo',
+    'password' => 'Contraseña',
+    'state' => 'Estado',
+    'address' => 'Dirección',
+];
