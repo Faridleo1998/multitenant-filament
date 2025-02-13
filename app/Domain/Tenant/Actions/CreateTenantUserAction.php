1f617bedfcc31d87f0462219ec2b808a2b2ef606
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-12 20:37:27 -0500

    feat: add roles argument and sync user roles in create tenant user action

--- app/Domain/Tenant/Actions/CreateTenantUserAction.php
+++ app/Domain/Tenant/Actions/CreateTenantUserAction.php
@@ -5,12 +5,13 @@ namespace App\Domain\Tenant\Actions;
 use App\Domain\Tenant\Models\Tenant;
 use App\Domain\User\Models\CentralUser;
 use App\Domain\User\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class CreateTenantUserAction
 {
-    public function execute(Tenant $tenant, CentralUser $centralUser): void
+    public function execute(Tenant $tenant, CentralUser $centralUser, Collection $roles): void
     {
-        $tenant->run(function () use ($centralUser) {
+        $tenant->run(function () use ($centralUser, $roles) {
             $user = User::withTrashed()->where('global_id', $centralUser->global_id)->first();
             if ($user) {
                 $user->restore();
@@ -22,6 +23,8 @@ class CreateTenantUserAction
                     'password' => $centralUser->password,
                 ]);
             }
+
+            $user->syncRoles($roles);
         });
     }
 }
