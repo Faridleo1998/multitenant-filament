8c3113fdcfb74f82ce197e16a9bf551fcdfe4995
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-14 10:39:22 -0500

    feat: add afterCreate method to create domain in create tenant

--- app/Filament/Resources/TenantResource/Pages/CreateTenant.php
+++ app/Filament/Resources/TenantResource/Pages/CreateTenant.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\TenantResource\Pages;
 
+use App\Domain\Tenant\Actions\CreateTenantDomainAction;
 use App\Filament\Resources\TenantResource;
 use Filament\Resources\Pages\CreateRecord;
 
@@ -10,4 +11,11 @@ class CreateTenant extends CreateRecord
     protected static string $resource = TenantResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    protected function afterCreate()
+    {
+        $createTenantDomainAction = app(CreateTenantDomainAction::class);
+        $tenant = $this->record;
+        $createTenantDomainAction->execute($tenant, $tenant->domain);
+    }
 }
