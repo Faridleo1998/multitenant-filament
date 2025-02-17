ac4d6b9d55e123e2a0f6cc3cda924d629ef45986
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-17 08:52:56 -0500

    feat: add tenant panel provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FilamentServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\TenantPanelProvider::class,
     App\Providers\PluginServiceProvider::class,
     App\Providers\TenancyServiceProvider::class,
 ];
