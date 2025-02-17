708617f7715f277d1be383177bf17371ddea435f
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-17 08:53:55 -0500

    feat: add role resource in tenant panel

---
+++ app/Filament/Tenant/Resources/RoleResource/Pages/ViewRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Tenant\Resources\RoleResource\Pages;
+
+use App\Filament\Tenant\Resources\RoleResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewRole extends ViewRecord
+{
+    protected static string $resource = RoleResource::class;
+}
