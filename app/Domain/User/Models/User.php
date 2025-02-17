d19fc94a19a6b18267a611ac82a864e219661e95
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-17 16:02:35 -0500

    feat: add canAccessPanel and canBeImpersonated methods in user model

--- app/Domain/User/Models/User.php
+++ app/Domain/User/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Domain\User\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Domain\User\Models\Attributes\UserAttributes;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 use Stancl\Tenancy\Contracts\Syncable;
 use Stancl\Tenancy\Database\Concerns\ResourceSyncing;
 
-class User extends Authenticatable implements Syncable
+class User extends Authenticatable implements FilamentUser, Syncable
 {
     use HasRoles, Notifiable, ResourceSyncing, SoftDeletes;
     use UserAttributes;
@@ -37,6 +39,16 @@ class User extends Authenticatable implements Syncable
             'email',
             'phone',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
+    public function canBeImpersonated(): bool
+    {
+        return true;
     }
 
     protected function casts(): array
