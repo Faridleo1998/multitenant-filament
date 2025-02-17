9435f3b4f72aa02db29dfa200deeed3244fabd26
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-17 18:12:45 -0500

    feat: change creation process in manage user in tenant panel

--- app/Filament/Tenant/Resources/UserResource/Pages/ManageUsers.php
+++ app/Filament/Tenant/Resources/UserResource/Pages/ManageUsers.php
@@ -2,10 +2,15 @@
 
 namespace App\Filament\Tenant\Resources\UserResource\Pages;
 
+use App\Domain\Tenant\Actions\CreateTenantUserAction;
+use App\Domain\User\Models\CentralUser;
+use App\Domain\User\Models\User;
 use App\Filament\Tenant\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 use Filament\Support\Enums\MaxWidth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class ManageUsers extends ManageRecords
 {
@@ -15,7 +20,25 @@ class ManageUsers extends ManageRecords
     {
         return [
             Actions\CreateAction::make()
-                ->modalWidth(MaxWidth::Small),
+                ->modalWidth(MaxWidth::Small)
+                ->using(
+                    function (
+                        array $data,
+                        CreateTenantUserAction $createTenantUserAction
+                    ): Model {
+
+                        $centralUser = CentralUser::firstOrCreate([
+                            'email' => $data['email'],
+                        ], [
+                            'name' => $data['name'],
+                            'password' => Hash::make($data['password']),
+                        ]);
+
+                        $createTenantUserAction->execute(tenancy()->tenant, $centralUser, $data['roles']);
+
+                        return User::where('global_id', $centralUser->global_id)->first();
+                    }
+                ),
         ];
     }
 }
