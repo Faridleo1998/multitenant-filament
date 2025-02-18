e411a78bda54283c418c24cdbdf1904ebe5a75cb
Farid Moreno <farid.leonardo98@gmail.com>
2025-02-17 19:14:33 -0500

    feat(tenant): add validation in category seeder

--- database/seeders/Tenant/CategorySeeder.php
+++ database/seeders/Tenant/CategorySeeder.php
@@ -9,7 +9,7 @@ class CategorySeeder extends Seeder
 {
     public function run(): void
     {
-        if (app()->isLocal()) {
+        if (app()->isLocal() && config('system.factories_count')) {
             Category::new()
                 ->count(config('system.factories_count'))
                 ->create();
